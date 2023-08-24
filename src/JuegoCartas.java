6736fe6743fc5c0937ad47622614dd3d35292581
Xiao <z.xiao01@ufromail.cl>
2023-08-24 16:29:40 -0400

    version 2.0

--- src/JuegoCartas.java
+++ src/JuegoCartas.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 public class JuegoCartas {
     public static void main(String[] args) {
     String[][] matriz= crearMatrizCartas();
@@ -8,7 +9,7 @@ public class JuegoCartas {
 
 
     public static String[][] crearMatrizCartas(){
-        String[][] matrizDeCartas= new String[2][12];
+        String[][] matrizDeCartas= new String[12][2];
         return matrizDeCartas;
     }
     public static String[][] agregarCartas(String [][] cartas){
@@ -19,6 +20,28 @@ public class JuegoCartas {
             }
         return cartas;
         }
+    public static String[][] obtenerCarta(String[][] cartas){
+        Random random = new Random();
+        int filaAleatoria = random.nextInt(cartas.length);
+
+        String[][] carta = new String [2][1];
+        carta [0][0] = cartas[filaAleatoria][0];
+        carta [1][0] = cartas[filaAleatoria][1];
+
+
+
+
+    return carta;
+    }
+    /* como esta parte no esta completo lo dejo como comentario para no alterar el funcionnamiento del programa
+
+    public static String[][] Jugar(){
+        for (int contaodr=0;contaodr<3;contaodr++){
+        }
+
+    }
+
+    */
 
 
 

a3694b21188e55a1bfef8d68292c16553e09d301
Xiao <z.xiao01@ufromail.cl>
2023-08-24 15:31:20 -0400

    version 1.0

--- src/JuegoCartas.java
+++ src/JuegoCartas.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
 public class JuegoCartas {
+    public static void main(String[] args) {
+    String[][] matriz= crearMatrizCartas();
+    String[][] matriz1=agregarCartas(matriz);
+        System.out.println(matriz1);
+
+    }
+    public static String[][] crearMatrizCartas(){
+        String[][] matrizDeCartas= new String[2][12];
+        return matrizDeCartas;
+    }
+    public static String[][] agregarCartas(String cartas[][]){
+        Scanner matriz = new Scanner(System.in);
+        for (int fila=0;fila<2;fila++){
+            matriz[fila][o]= matriz.nextLine();
+            matriz[fila][1]=matriz.nextLine();
+            }
+        return cartas;
+        }
+
+
+
+    }
 
 }
